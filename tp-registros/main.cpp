e0c8b7a07705b13c15598835bcc8a722f5cc2c9a
Alan-Vaca <70666417+Alan-Vaca@users.noreply.github.com>
2020-09-17 22:01:24 -0300

    Add files via upload

---
+++ tp-registros/main.cpp
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cstring>
+#include <cstdio>
+#include <ctime>
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);                              ///FUNCION PARA PINTAR
+using namespace std;
+#include "includes/prototipos.h"
+#include "includes/visuales.h"
+#include "includes/menues.h"
+#include "includes/usuarios.h"
+#include "includes/entrenamiento.h"
+                                                                ///LISTARA LA ID DEL USUARIO
+
+
+int main(){                                                                     //MAIN PRINCIPAL
+    SetConsoleTextAttribute(hConsole, 6);
+    recuadro(26,10,46,21);
+    SetConsoleTextAttribute(hConsole, 7);
+    MenuPrincipal();
+    return 0;
+}
